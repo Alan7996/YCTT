293ad13ffcd1e408e8b414027fbe4d5698bf1b61
Alan7996 <my_ant1@naver.com>
2016-10-06 13:10:42 +0800

    Initial Commit

---
+++ YCTT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YCTT
+//
+//  Created by 수현 on 10/6/16.
+//  Copyright © 2016 YCIS-PX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
